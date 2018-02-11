b43703b1c385b4960bf6b90990ae8d6d81c475d7
viktor-miller <phpfriq@gmail.com>
2018-02-11 13:39:01 +0100

    lang file fixed

--- resources/lang/en/alert.php
+++ resources/lang/en/alert.php
@@ -1,12 +1,8 @@
 <?php
 
 return [
-    'success' => [
-        'sent' => 'A confirmation link has been sent. Please check your email.',
-        'confirmed' => 'You have successfully verified your email!'
-    ],
-    'fail' => [
-        'user' => 'Your email cannot be found or is already confirmed',
-        'token' => 'Your confirmation token is invalid. Please try again'
-    ]
+    'sent' => 'A confirmation link has been sent. Please check your email.',
+    'confirmed' => 'You have successfully verified your email!',
+    'user' => 'Your email cannot be found or is already confirmed',
+    'token' => 'Your confirmation token is invalid. Please try again'
 ];
